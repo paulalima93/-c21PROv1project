c8ffdbe38a1067873e156457af63b9f1fe40f009
paulalima93 <91716314+paulalima93@users.noreply.github.com>
2022-08-13 16:36:12 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,51 +1,79 @@
-var tela;
-var bloco1,bloco2,bloco3,bloco4;
-var bola, arestas;
+var canvas;
+var block1,block2,block3,block4;
+var ball, edges;
 var music;
 
 function preload(){
-    // carregue o som aqui
+    music = loadSound("music.mp3");
 }
 
+
 function setup(){
-    tela = createCanvas(800,600);
+    canvas = createCanvas(800,600);
 
-    bloco1 = createSprite(0,580,360,30);
-    bloco1.shapeColor = "blue";
+    block1 = createSprite(0,580,360,30);
+    block1.shapeColor = "blue";
 
-    bloco2 = createSprite(295,580,200,30);
-    bloco2.shapeColor = "orange";
+    block2 = createSprite(295,580,200,30);
+    block2.shapeColor = "orange";
 
-    //crie aqui mais dois blocos, ou seja, bloco3 e bloco4
+    block3 = createSprite(515,580,200,30);
+    block3.shapeColor = "red";
 
-    bola = createSprite(random(20,750),100, 40,40);
-    bola.shapeColor = rgb(255,255,255);
-    //escreva o código para adicionar velocityX e velocityY à bola
+    block4 = createSprite(740,580,220,30);
+    block4.shapeColor = "green";
+
+    ball = createSprite(random(20,750),100, 40,40);
+    ball.shapeColor = rgb(255,255,255);
+    
+    // dar velocidade à bola
+    /*
+    ball = velocityX(4);
+    ball = velocityY (9);
+    */
+     /*
+    velocityX = 4;
+    velocityY = 9;
+    */
+     /*
+    ball.velocity.x = 4;
+    ball.velocity.y = 9;
+    */
+     /*
+    ball.velocityX = 4;
+    ball.velocityY = 9;
+    */
 
 }
 
 function draw() {
     background(rgb(169,169,169));
-   
+    edges=createEdgeSprites();
+    ball.bounceOff(edges);
 
-    
-    //escreva o código de ricochete de bola para bloco1 
-    if(bloco1.isTouching(bola) && bola.bounceOff(bloco1)){
-        bola.shapeColor = "blue";
+    if(block1.isTouching(ball) && ball.bounceOff(block1)){
+        ball.shapeColor = "blue";
         music.play();
     }
 
-
-    if(bloco2.isTouching(bola)){
-        bola.shapeColor = "orange";
-        //escreva o código para definir velocityX e velocityY da bola como 0
-
-        //escreva o código para parar a música
+    if(block2.isTouching(ball)){
+        ball.shapeColor = "orange";
+        ball.velocityX = 0;
+        ball.velocityY = 0;
+        music.stop();
     }
 
-    //escreva o código de ricochete de bola para bloco3
+    if(block3.isTouching(ball) && ball.bounceOff(block3)){
+        ball.shapeColor = "red";
+    }
 
-    //escreva o código de ricochete de bola para bloco4
+    if(block4.isTouching(ball) && ball.bounceOff(block4)){
+        //deixar a bola verde
+        //ball.shapeColor = "green";
+        //ball = shapeColor(green);
+        //ball = shapeColor("green");
+        //ball.shapeColor = green;
+    }
 
     drawSprites();
 }
